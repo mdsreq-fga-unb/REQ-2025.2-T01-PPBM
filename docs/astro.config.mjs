1bfc0be13d3f15604dd7f3ec73f94b8c18d24dfe
Vitor Trancoso <studyvitorm@gmail.com>
2025-09-26 00:51:25 -0300

    refactor(docs): Atualiza título de "Levantamento Parcial de Requisitos" para "Requisitos"
    Modifica o título do documento para uma versão mais concisa, melhorando a clareza e a consistência na documentação dos requisitos.

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -49,7 +49,7 @@ export default defineConfig({
 							slug: "visao/estrategias",
 						},
 						{
-							label: "Levantamento Parcial de Requisitos",
+							label: "Requisitos",
 							slug: "visao/levantamento-parcial",
 						},
 						{
