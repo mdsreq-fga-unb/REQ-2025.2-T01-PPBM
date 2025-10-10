b1dd8c21db6abe4106d6779a201494c424588f13
PhiLipe Morais <liperapltda@gmail.com>
2025-10-09 23:12:02 -0300

    feat: Adiciona suporte a Tailwind CSS, integrações com React e novos componentes de UI, incluindo tabela, botão e alerta

---
+++ docs/src/components/ExtraPage/index.jsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+const ExtraPage = () => {
+    return (
+        <Button className="text-red-500" onClick={() => console.log('teste')}>
+             Teste
+        </Button>
+    )
+}
+
+export default ExtraPage;
