c460d8b319667a8f5b57cbf17942f8b9d2d72bbb
Vitor Trancoso <studyvitorm@gmail.com>
2025-11-29 13:13:53 -0300

    Update Astro config and content asset mappings
    Added 'site' property to astro.config.mjs and updated the config digest in data-store.json. Adjusted import order and asset mapping in content-assets.mjs to reflect new or reordered blog post assets.

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -7,6 +7,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ppbm.no-fluxo.com',
   output: 'static',
   integrations: [svelte(), tailwind(), mdx()],
   server: {
